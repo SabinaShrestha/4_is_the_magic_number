91efd60200e1d691b595b69b0bde3d6f865a5c9e
Sabina Shrestha <sabina.pari@yahoo.com>
2018-06-01 10:39:02 -0600

    printing the sequence of magic nunber 4

--- four_magic_number.rb
+++ four_magic_number.rb
@@ -6,8 +6,9 @@ def magic_number
   num = gets.to_i
   number = num.to_words
   until number == "four"
-    num1 = number.length
-    puts "#{number} is #{num1}"
+    num1 = number.delete("-").length
+    puts "#{num} is #{num1}"
+    num = num1
     number = num1.to_words
     if num1 == 4
       puts "4 is a magic number."
